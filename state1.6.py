4be98d6a73c19c89772218bb640eff9e732df174
Ayabongayb <Ayabongayb@gmail.com>
2021-12-13 22:51:04 +0200

    added state1.5 and 1.6 of the project

---
+++ state1.6.py
@@ -0,0 +1,12 @@
+def computepay(h, r):
+    if h <= 40:
+        pay = h * r
+    elif h > 40:
+        pay = (40*r)+(h-40)*(1.5*r)
+        
+    return pay
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+p = computepay(hrs, rate)
+print("Pay", p)
